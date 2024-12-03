de90d137d78ae733a4814b634418a99014bf9fe5
Phạm Đức Bình <binhpd@oeg.vn>
2024-12-03 12:22:18 +0700

    Update Form Login Page

--- src/pages/public/login/index.tsx
+++ src/pages/public/login/index.tsx
@@ -1,17 +1,18 @@
 import { LockOutlined, UserOutlined } from '@ant-design/icons'
-import { Button, Card, Checkbox, Form, Input } from 'antd'
+import { Button, Card, Checkbox, Form, Input, message } from 'antd'
 import './LoginPage.scss'
 
 export default function LoginPage(): JSX.Element {
   const onFinish = (values: any) => {
     console.log('Success:', values)
+    message.success('Đăng nhập thành công')
   }
 
   return (
     <div className='login-container'>
       <Card
         className='login-card'
-        title={<h2 style={{ textAlign: 'center', fontSize: '24px', padding: '20px' }}>Đăng nhập</h2>}
+        title={<h2 style={{ textAlign: 'center', fontSize: '24px', padding: '20px' }}>Đăng Nhập</h2>}
         bordered={false}
       >
         <Form
@@ -22,36 +23,38 @@ export default function LoginPage(): JSX.Element {
           onFinish={onFinish}
         >
           <Form.Item
+            className='form-item'
             name='username'
             rules={[
               {
                 required: true,
-                message: 'Please input your Username!'
+                message: 'Bắt buộc nhập tài khoản !'
               }
             ]}
           >
-            <Input prefix={<UserOutlined />} placeholder='Username' size='large' className='custom-input' />
+            <Input prefix={<UserOutlined />} placeholder='Tài khoản' size='large' className='custom-input' />
           </Form.Item>
           <Form.Item
+            className='form-item'
             name='password'
             rules={[
               {
                 required: true,
-                message: 'Please input your Password!'
+                message: 'Bắt buộc nhập mật khẩu !'
               }
             ]}
           >
             <Input
               prefix={<LockOutlined />}
               type='password'
-              placeholder='Password'
+              placeholder='Mật khẩu'
               size='large'
               className='custom-input'
             />
           </Form.Item>
           <Form.Item>
             <Form.Item name='remember' valuePropName='checked' noStyle>
-              <Checkbox>Remember</Checkbox>
+              <Checkbox>Nhớ đăng nhập</Checkbox>
             </Form.Item>
           </Form.Item>
 
