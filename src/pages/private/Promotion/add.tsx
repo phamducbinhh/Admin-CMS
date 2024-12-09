a686aaf42292059b581d5f4d83b9db961629367b
hungtech511 <vhung.webdev@gmail.com>
2024-12-09 11:38:59 +0700

    finish crud promotion

--- src/pages/private/Promotion/add.tsx
+++ src/pages/private/Promotion/add.tsx
@@ -1,6 +1,7 @@
-import { useAddPromotionMutation } from '@/queries/promotions'
+import { useAddPromotionMutation, useAddPromotionToAllUserMutation } from '@/queries/promotions'
 import { DataTypeVehicle } from '@/types/DataType'
 import { fieldModalTable } from '@/utils/fieldModalTable'
+import { PlusOutlined } from '@ant-design/icons'
 import { Form, Button, message } from 'antd'
 import { HttpStatusCode } from 'axios'
 // import TextArea from 'antd/es/input/TextArea'
@@ -10,6 +11,7 @@ import { useNavigate } from 'react-router-dom'
 const AddPromotionPage: React.FC = () => {
   const [form] = Form.useForm()
   const addMutation = useAddPromotionMutation()
+  const addAllMutation = useAddPromotionToAllUserMutation()
   const navigate = useNavigate()
 
   const filteredFields = fieldModalTable.filter(
@@ -36,6 +38,23 @@ const AddPromotionPage: React.FC = () => {
     }
   }
 
+  const handleAddPromotionToAllUser = async () => {
+    const values = form.getFieldsValue()
+
+    try {
+      const response = await addAllMutation.mutateAsync(values)
+
+      if (response.status === HttpStatusCode.Created) {
+        message.success('Create promotion success')
+        navigate('/promotion')
+      } else {
+        message.error(response.message)
+      }
+    } catch (error) {
+      console.error('Error values:', error)
+    }
+  }
+
   return (
     <Form onFinish={handleFormSubmit} form={form} layout='vertical'>
       {filteredFields.map((field) => (
@@ -50,6 +69,15 @@ const AddPromotionPage: React.FC = () => {
       ))}
       <Button type='primary' htmlType='submit'>
         Add
+      </Button>
+      <Button
+        onClick={() => handleAddPromotionToAllUser()}
+        style={{ marginLeft: 16 }}
+        type='primary'
+        icon={<PlusOutlined />}
+        ghost
+      >
+        Thêm promotion cho tất cả user
       </Button>
     </Form>
   )
