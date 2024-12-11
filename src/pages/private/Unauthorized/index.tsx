ad351a7bc8169c7ee3a4dfd4934827576b728478
Phạm Đức Bình <binhpd@oeg.vn>
2024-12-11 10:16:44 +0700

    Update Authorization

--- src/pages/private/Unauthorized/index.tsx
+++ src/pages/private/Unauthorized/index.tsx
@@ -1,34 +1,19 @@
-import { Button, Card, Space, Typography } from 'antd'
+import { Button, Result } from 'antd'
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
-
-const { Title, Text } = Typography
+import { Link } from 'react-router-dom'
 
 const UnauthorizedPage: React.FC = () => {
-  const navigate = useNavigate()
-
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-        backgroundColor: '#f5f5f5'
-      }}
-    >
-      <Card style={{ maxWidth: 400, textAlign: 'center', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)' }} bordered={false}>
-        <Title level={2} style={{ color: '#ff4d4f' }}>
-          403 - Forbidden
-        </Title>
-        <Text type='secondary'>Bạn không có quyền truy cập vào tài nguyên này.</Text>
-        <Space direction='vertical' size='middle' style={{ marginTop: 20, width: '100%' }}>
-          <Button type='default' block onClick={() => navigate('/')}>
-            Về Trang chủ
-          </Button>
-        </Space>
-      </Card>
-    </div>
+    <Result
+      status='403'
+      title='403'
+      subTitle='Sorry, you are not authorized to access this page.'
+      extra={
+        <Link to='/'>
+          <Button type='primary'>Back Home</Button>
+        </Link>
+      }
+    />
   )
 }
 
