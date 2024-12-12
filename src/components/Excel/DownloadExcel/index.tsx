9640fd80b4a0fd0329200c00ce4416541a540fbb
hungtech511 <vhung.webdev@gmail.com>
2024-12-12 16:50:37 +0700

    fix features

---
+++ src/components/Excel/DownloadExcel/index.tsx
@@ -0,0 +1,65 @@
+import TemplateTripConvenience from '@/assets/Template_Trip_Convenience.xlsx'
+import TemplateInterProvincial from '@/assets/Template_Inter_Provincial.xlsx'
+
+import { Button, Select } from 'antd'
+
+interface DownloadExcelProps {
+  exportedFile: boolean
+  setExportedFile: (value: boolean) => void
+  selectedOption: string
+  setSelectedOption: (value: string) => void
+}
+
+const DownloadExcel: React.FC<DownloadExcelProps> = ({
+  selectedOption,
+  setSelectedOption,
+  exportedFile,
+  setExportedFile
+}) => {
+  const { Option } = Select
+
+  const downloadFile = async () => {
+    try {
+      const fileUrl = selectedOption === '1' ? TemplateInterProvincial : TemplateTripConvenience
+      const fileName = selectedOption === '1' ? 'Template_Inter_Provincial.xlsx' : 'Template_Trip_Convenience.xlsx'
+      const response = await fetch(fileUrl)
+      const blob = await response.blob()
+      const url = window.URL.createObjectURL(blob)
+      const link = document.createElement('a')
+      link.href = url
+      link.download = fileName
+      document.body.appendChild(link)
+      link.click()
+      link.remove()
+      setExportedFile(true)
+    } catch (error) {
+      console.error('Error downloading the file:', error)
+    }
+  }
+
+  const handleSelectChange = (value: string) => {
+    setSelectedOption(value)
+  }
+
+  return (
+    <div>
+      <Select
+        disabled={exportedFile}
+        style={{ width: 200, marginRight: 20 }}
+        placeholder='Choose an option'
+        onChange={handleSelectChange}
+      >
+        <Option value='1'>Template Trip Inter Provincial</Option>
+        <Option value='2'>Template Trip Convenience</Option>
+      </Select>
+
+      {selectedOption && (
+        <Button style={{ marginRight: 20 }} disabled={exportedFile} onClick={downloadFile}>
+          Download Template Trip
+        </Button>
+      )}
+    </div>
+  )
+}
+
+export default DownloadExcel
--- src/pages/private/trips/excel.tsx
+++ src/pages/private/trips/excel.tsx
@@ -1,161 +1,28 @@
-/* eslint-disable react-refresh/only-export-components */
 import React, { useState } from 'react'
-import { Button, message, Select, Table } from 'antd'
+import { Button, message, Table } from 'antd'
 
 // import { useAddVehiclesFromExcelMutation, useImportExcel } from '@/queries/vehicle'
 
 import { HttpStatusCode } from 'axios'
-import { useNavigate } from 'react-router-dom'
-
-import TemplateTripConvenience from '@/assets/Template_Trip_Convenience.xlsx'
-import TemplateInterProvincial from '@/assets/Template_Inter_Provincial.xlsx'
+// import { useNavigate } from 'react-router-dom'
 import { useImportExcel } from '@/queries/trip'
-
-interface DownloadExcelProps {
-  exportedFile: boolean
-  setExportedFile: (value: boolean) => void
-  selectedOption: string
-  setSelectedOption: (value: string) => void
-}
-
-export const processValidEntriesAndColumns = (responseData: any) => {
-  const entries = responseData.map((item: any) => {
-    // Extract and flatten pointStartDetail
-    const pointStartDetail = item.pointStartDetail || {}
-    const flattenedDetails = Object.entries(pointStartDetail).reduce(
-      (acc, [key, value], detailIndex) => ({
-        ...acc,
-        [`point_start_${detailIndex}`]: key,
-        [`start_time_${detailIndex}`]: value
-      }),
-      {}
-    )
-
-    // Exclude unwanted keys and include flattened details
-    const filteredItem = Object.fromEntries(
-      Object.entries(item).filter(
-        ([key]) =>
-          ![
-            'id',
-            'status',
-            'typeOfTrip',
-            'pointStartDetail',
-            'pointEndDetail',
-            'createdAt',
-            'createdBy',
-            'errorMessages'
-          ].includes(key)
-      )
-    )
-
-    return {
-      ...filteredItem,
-      ...flattenedDetails
-    }
-  })
-
-  // Dynamically create columns
-  const dynamicColumns = entries.length
-    ? [
-        // Static columns
-        ...Object.keys(entries[0])
-          .filter((key) => !key.startsWith('point_start_') && !key.startsWith('start_time_'))
-          .map((key) => ({
-            title: key.charAt(0).toUpperCase() + key.slice(1),
-            dataIndex: key,
-            key,
-            width: '550px'
-          })),
-
-        // Dynamic columns for point_start and start_time
-        ...Array.from(
-          new Set(
-            Object.keys(entries[0])
-              .filter((key) => key.startsWith('point_start_') || key.startsWith('start_time_'))
-              .map((key) => key.match(/\d+$/)?.[0]) // Extract the index
-          )
-        )
-          .sort((a, b) => Number(a) - Number(b)) // Sort indices numerically
-          .flatMap((index) => [
-            {
-              title: `Point Start (${Number(index) + 1})`,
-              dataIndex: `point_start_${index}`,
-              key: `point_start_${index}`,
-              width: '550px'
-            },
-            {
-              title: `Start Time (${Number(index) + 1})`,
-              dataIndex: `start_time_${index}`,
-              key: `start_time_${index}`,
-              width: '550px'
-            }
-          ])
-      ]
-    : []
-
-  return { entries, dynamicColumns }
-}
-
-const DownloadExcel: React.FC<DownloadExcelProps> = ({
-  selectedOption,
-  setSelectedOption,
-  exportedFile,
-  setExportedFile
-}) => {
-  const { Option } = Select
-
-  const downloadFile = async () => {
-    try {
-      const fileUrl = selectedOption === '1' ? TemplateInterProvincial : TemplateTripConvenience
-      const fileName = selectedOption === '1' ? 'Template_Inter_Provincial.xlsx' : 'Template_Trip_Convenience.xlsx'
-      const response = await fetch(fileUrl)
-      const blob = await response.blob()
-      const url = window.URL.createObjectURL(blob)
-      const link = document.createElement('a')
-      link.href = url
-      link.download = fileName
-      document.body.appendChild(link)
-      link.click()
-      link.remove()
-      setExportedFile(true)
-    } catch (error) {
-      console.error('Error downloading the file:', error)
-    }
-  }
-
-  const handleSelectChange = (value: string) => {
-    setSelectedOption(value)
-  }
-
-  return (
-    <div>
-      <Select
-        disabled={exportedFile}
-        style={{ width: 200, marginRight: 20 }}
-        placeholder='Choose an option'
-        onChange={handleSelectChange}
-      >
-        <Option value='1'>Template Trip Inter Provincial</Option>
-        <Option value='2'>Template Trip Convenience</Option>
-      </Select>
-
-      {selectedOption && (
-        <Button style={{ marginRight: 20 }} disabled={exportedFile} onClick={downloadFile}>
-          Download Template Trip
-        </Button>
-      )}
-    </div>
-  )
-}
+import { processResponseData } from '@/utils/excelColumnDynamic'
+import DownloadExcel from '@/components/Excel/DownloadExcel'
 
 const ExcelTripPage = () => {
   const [selectedOption, setSelectedOption] = useState('1')
   const [exportedFile, setExportedFile] = useState(true)
   const [fileName, setFileName] = useState('')
-  const [data, setData] = useState({ validEntries: [], invalidEntries: [], hasData: false })
+  const [data, setData] = useState({
+    showValidEntries: [],
+    showInvalidEntries: [],
+    hasData: false,
+    validEntries: [],
+    invalidEntries: []
+  })
   const addMutation = useImportExcel()
   // const importVehicleFromExcelMutation = useAddVehiclesFromExcelMutation()
-  const navigate = useNavigate()
+  // const navigate = useNavigate()
 
   const [columns, setColumns] = useState<any[]>([])
 
@@ -178,19 +45,21 @@ const ExcelTripPage = () => {
       if (response.status === HttpStatusCode.Ok) {
         message.success('File uploaded successfully')
 
-        const validData = processValidEntriesAndColumns(response.data.validEntries)
-        const inValidData = processValidEntriesAndColumns(response.data.invalidEntries)
+        const validData = processResponseData(response.data.validEntries)
+        const inValidData = processResponseData(response.data.invalidEntries)
 
         setData({
           hasData: true,
-          validEntries: validData.entries,
-          invalidEntries: inValidData.entries
+          showValidEntries: validData.entries,
+          showInvalidEntries: inValidData.entries,
+          validEntries: response.data.validEntries,
+          invalidEntries: response.data.invalidEntries
           // invalidEntries: response.data.invalidEntries.map((item: any, index: any) => ({
           //   ...item,
           //   key: index
           // }))
         })
-        setColumns(validData.dynamicColumns)
+        setColumns(validData.updatedColumns)
       } else {
         message.error(response)
       }
@@ -217,6 +86,8 @@ const ExcelTripPage = () => {
       console.error('Error values:', error)
     }
   }
+
+  console.log(data.invalidEntries)
 
   return (
     <div>
@@ -254,12 +125,12 @@ const ExcelTripPage = () => {
       )}
 
       <div style={{ marginTop: 20 }}>
-        {data.validEntries.length > 0 && (
+        {data.showValidEntries.length > 0 && (
           <>
             <h2 style={{ marginBottom: 20 }}>Valid Data</h2>
             <Table
               columns={columns}
-              dataSource={data.validEntries}
+              dataSource={data.showValidEntries}
               bordered
               pagination={{ pageSize: 5 }}
               scroll={{ x: 400 }}
@@ -267,25 +138,25 @@ const ExcelTripPage = () => {
           </>
         )}
 
-        {data.invalidEntries.length > 0 && (
+        {data.showInvalidEntries.length > 0 && (
           <>
             <h2 style={{ fontSize: 24, marginBottom: 20 }}>Invalid Data</h2>
-            <Table columns={columns} dataSource={data.invalidEntries} bordered pagination={{ pageSize: 5 }} />
+            <Table columns={columns} dataSource={data.showInvalidEntries} bordered pagination={{ pageSize: 5 }} />
             <div>
-              {/* {data.invalidEntries.map((entry: any, index) =>
-                entry.errors.length > 0 ? (
+              {data.invalidEntries.map((entry: any, index) =>
+                entry.errorMessages.length > 0 ? (
                   <p style={{ marginBottom: 20 }} key={index}>
-                    {entry.errors}
+                    {entry.errorMessages}
                   </p>
                 ) : null
-              )} */}
+              )}
             </div>
           </>
         )}
       </div>
 
       {data.hasData && data.invalidEntries.length === 0 && (
-        <Button onClick={() => handleImportToVehicleList()}>Import to Vehicle List</Button>
+        <Button onClick={() => handleImportToVehicleList()}>Import to Trip List</Button>
       )}
     </div>
   )
