615e48619baaafaae9ecde392f4294787c950a95
Phạm Đức Bình <binhpd@oeg.vn>
2024-12-12 17:10:00 +0700

    Update Approve

--- src/pages/private/Ticket-Not-Paid/index.tsx
+++ src/pages/private/Ticket-Not-Paid/index.tsx
@@ -43,7 +43,8 @@ const TicketNotPaidPage: React.FC = () => {
     return {
       ...item,
       key: item.ticketId || item.someUniqueField,
-      ticketId: ticketDetails?.description
+      ticketId: ticketDetails?.description,
+      id: item.ticketId
     }
   })
 
