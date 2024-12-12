a7ce035e6259f77c36a85e39d5cd62f095c0666c
Phạm Đức Bình <binhpd@oeg.vn>
2024-12-12 14:37:24 +0700

    Update Vehicle

--- src/pages/private/History-Vehicle/index.tsx
+++ src/pages/private/History-Vehicle/index.tsx
@@ -3,7 +3,9 @@ import useColumnSearch from '@/hooks/useColumnSearch'
 import { useQueryHistoryRentVehicle } from '@/queries/history'
 import { handlingTsUndefined } from '@/utils/handlingTsUndefined'
 import renderWithLoading from '@/utils/renderWithLoading'
-import { Table, TableProps } from 'antd'
+import { SearchOutlined } from '@ant-design/icons'
+import { Button, DatePicker, Table, TableProps } from 'antd'
+import dayjs from 'dayjs'
 import React from 'react'
 
 interface DataType {
@@ -14,6 +16,7 @@ interface DataType {
   timeEnd: number
   price: number
   carOwner: string
+  createdAt: string // ISO string for date-time
 }
 
 const HistoryRentVehiclePage: React.FC = () => {
@@ -47,20 +50,56 @@ const HistoryRentVehiclePage: React.FC = () => {
       width: '20%'
     },
     {
-      title: 'Thời gian bắt đầu',
-      dataIndex: 'createdAt',
-      key: 'createdAt',
-      align: 'center',
-      render: (date) => <span>{formatTime(date) ?? 'null'}</span>,
-      width: '20%'
-    },
-    {
       title: 'Biển số xe',
       dataIndex: 'licenseVehicle',
       key: 'licenseVehicle',
       align: 'center',
       ...useColumnSearch().getColumnSearchProps('licenseVehicle'),
       width: '20%'
+    },
+    {
+      title: 'Thời gian',
+      dataIndex: 'createdAt',
+      key: 'createdAt',
+      align: 'center',
+      filterDropdown: ({ setSelectedKeys, confirm, clearFilters }) => (
+        <div style={{ padding: 8 }}>
+          <DatePicker.RangePicker
+            onChange={(dates, dateStrings: any) => {
+              if (dates) {
+                setSelectedKeys([dateStrings])
+              } else {
+                setSelectedKeys([])
+              }
+            }}
+            format='YYYY-MM-DD'
+            style={{ marginBottom: 8, display: 'block' }}
+          />
+          <div>
+            <Button
+              type='primary'
+              onClick={confirm as any}
+              icon={<SearchOutlined />}
+              size='small'
+              style={{ width: 90, marginRight: 8 }}
+            >
+              Search
+            </Button>
+            <Button onClick={clearFilters as any} size='small' style={{ width: 90 }}>
+              Reset
+            </Button>
+          </div>
+        </div>
+      ),
+      onFilter: (value, record) => {
+        if (Array.isArray(value) && value[0]) {
+          const [startDate, endDate] = (value[0] as string).split(',')
+          const recordDate = dayjs(record.createdAt)
+          return recordDate.isAfter(dayjs(startDate)) && recordDate.isBefore(dayjs(endDate))
+        }
+        return false
+      },
+      render: (text) => <span>{formatTime(text)}</span>
     }
   ]
   const dataSource = data?.paymentRentVehicelDTOs?.map((item: any) => ({
