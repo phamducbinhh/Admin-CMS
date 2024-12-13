5476134c8fa8e6b69868b7fce645d1d28e0c5118
Phạm Đức Bình <binhpd@oeg.vn>
2024-12-13 10:57:40 +0700

    Update Done Sidebar

---
+++ src/utils/rolePath.ts
@@ -0,0 +1,40 @@
+export const allowAdminRoute = ['account', 'role', 'driver', 'request']
+
+export const allowStaffRoute = [
+  'dashboard',
+  'trips',
+  'vehicles',
+  'vehicles-owner',
+  'promotion',
+  'driver',
+  'history-rent-vehicle',
+  'history-rent-driver',
+  'cost-type',
+  'fixed-cost',
+  'request',
+  'revenue',
+  'ticket',
+  'ticket-not-paid',
+  'reviews'
+]
+
+export const allowDriverRoute = [
+  'vehicles',
+  'vehicles-using',
+  'history-rent-vehicle',
+  'history-rent-driver',
+  'request',
+  'ticket',
+  'ticket-not-paid'
+]
+
+export const allowVehicleOwnerRoute = [
+  'dashboard',
+  'vehicles',
+  'history-rent-vehicle',
+  'history-rent-driver',
+  'request',
+  'revenue',
+  'ticket',
+  'total-ticket'
+]
